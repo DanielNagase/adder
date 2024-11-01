b6c0766b1cd1ee21d6c07254c66a246c2473f45e
Daniel Nagase <daniel.nagase@unity3d.com>
2024-11-01 15:16:21 +0900

    Start adding logic for walking a directory tree
    The goal is to walk a directory tree and call a function to process
    each entry that is a file. We want to traverse the tree in the same
    order as the find comand (a depth-first search).

--- adder.py
+++ adder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import argparse
+import argparse, os
 
 class FileSet:
 	'''Class for a set of files'''
@@ -16,14 +16,28 @@ class FileSet:
 		self.fileCount = fileCount
 		self.diskSize = diskSize
 
+def visitFile(entry):
+	print(entry.path)
+
+def visitPath(path):
+	entries = sorted(os.scandir(path), key=lambda entry: entry.name)
+
+	for entry in entries:
+		if entry.is_dir():
+			visitPath(entry)
+		elif entry.is_file():
+			visitFile(entry)
+
 def main():
 	parser = argparse.ArgumentParser(
 		description='A tool for adding files to git in chunks'
 	)
+	parser.add_argument('path', help='Path to add')
 	parser.add_argument('-s', '--size', help='Maximum size of a chunk (in MB)')
 	parser.add_argument('-c', '--count', help='Maximum number of files in a chunk')
 
 	args = parser.parse_args()
+	visitPath(args.path)
 
 if __name__ == "__main__":
 	main()
